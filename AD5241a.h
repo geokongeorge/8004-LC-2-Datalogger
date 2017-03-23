e75e086e942947dea1f9b38d484f817eb6b4f337
gmoore <gmoore@eng01gm.geokon.com>
2017-03-23 11:47:13 -0400

    CRC computation complete

---
+++ AD5241a.h
@@ -0,0 +1,10 @@
+#ifndef AD5241a_H
+#define AD5241a_H
+
+
+
+//************************PROTOTYPES*************************************
+void write_AD5241(unsigned char tap);                                           //Write 8 bit value to the AD5241 digital pot
+unsigned char read_AD5241(void);                                                //Read 8 bit value from AD5241 digital pot
+
+#endif
