d3401fe60714f1c0c97a03cd3b10474c97c0ba62
gmoore <gmoore@eng01gm.geokon.com>
2017-05-23 15:34:28 -0400

    Debugged Log Intervals

--- LC8004extFRAM_i.h
+++ LC8004extFRAM_i.h
@@ -7,10 +7,12 @@
 void displayError(unsigned int fram);
 unsigned int read_Int_FRAM(unsigned long absAdd);		
 void read_Flt_FRAM(unsigned long absAdd, unsigned int x);	
-long read_longFRAM(unsigned long add);                                          
+//long read_longFRAM(unsigned long add);                                        REM REV I
+unsigned long read_longFRAM(unsigned long add);                                 //REV I
 void write_Int_FRAM(unsigned long absAdd, unsigned int data);                 
 void write_Flt_FRAM(unsigned long absAdd, float f);                           
-void write_longFRAM(long f, unsigned long add);                                 
+//void write_longFRAM(long f, unsigned long add);                               REM REV I
+void write_longFRAM(unsigned long f, unsigned long add);                        //REV I
 //void selectBank(unsigned long absAdd, unsigned char x);                       //REM REV G
 unsigned char selectBank(unsigned long absAdd, unsigned char x);                //REV G
 void sleepFRAM(void);
