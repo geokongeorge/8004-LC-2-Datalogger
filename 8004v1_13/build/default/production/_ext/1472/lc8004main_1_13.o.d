12fa841dabe91a2c9000e5984c5dba0cc149a087
GMOORE <GMOORE@gmlab01.geokon.com>
2017-11-30 09:40:06 -0500

    Add automatic write to MODBUS firmware revision register the current firmware revision on bootup.

---
+++ 8004v1_13/build/default/production/_ext/1472/lc8004main_1_13.o.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/1472/lc8004main_1_13.o:  \
+ ../lc8004main_1_13.c  \
+../LC8004extFRAM_i.h  \
+../LC8004main_1_13.h  \
+ ../LC8004delay_b.h  \
+../AD5241b.h  \
+../FRAM_ADDRESSh.h 
